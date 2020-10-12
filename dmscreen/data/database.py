5b7f4d269855ab80dd3b367f7cf76846d388c5b1
Tom-Todd <thomas.m.a.todd@gmail.com>
2020-10-12 14:32:18 +0900

    Database: Ability Score Set
    +Add ability score set to database

--- dmscreen/data/database.py
+++ dmscreen/data/database.py
@@ -8,16 +8,23 @@ from sqlite3 import Error
 class DatabaseConnector():
     conn = None
 
-    def create_connection():
+    def create_connection(self):
         """ create a database connection to SQLite database """
         try:
-            conn = sqlite3.connect(filename)
+            self.conn = sqlite3.connect(filename)
         except Error as e:
             print(e)
 
-    def create_table():
+    def create_table(self, create_table_sql):
         try:
-            c = conn.cursor()
+            c = self.conn.cursor()
             c.execute(create_table_sql)
         except Error as e:
             print(e)
+
+    def create_ability_score_set(self, ability_score_set):
+        sql = ''' INSERT INTO ability_score_set(strength,dexterity,constitution,intelligence,wisdom,charisma)
+              VALUES(?,?,?,?,?,?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, ability_score_set)
+        self.conn.commit()
