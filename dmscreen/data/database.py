d3716270de68b22d5e597c03e169b821e18a7ddf
Tom-Todd <thomas.m.a.todd@gmail.com>
2020-10-12 13:09:26 +0900

    Database: Create table
    +Create table method

--- dmscreen/data/database.py
+++ dmscreen/data/database.py
@@ -1,14 +1,23 @@
 import sqlite3
+import os
+dir = os.path.dirname(__file__)
+filename = os.path.join(dir, 'db.sqlite')
+
 from sqlite3 import Error
 
-class database_connector():
-
+class DatabaseConnector():
     conn = None
 
-    def create_connection(db_file):
+    def create_connection():
         """ create a database connection to SQLite database """
         try:
-            conn = sqlite3.connect(db_file)
+            conn = sqlite3.connect(filename)
         except Error as e:
             print(e)
-        return conn
+
+    def create_table():
+        try:
+            c = conn.cursor()
+            c.execute(create_table_sql)
+        except Error as e:
+            print(e)
