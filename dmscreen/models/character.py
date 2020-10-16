1cf83c0c624435a319032d3157f91711d34f113a
Tom Todd <thomas.m.a.todd@gmail.com>
2020-10-16 19:04:02 +0900

    Add Hit points to Character
    Add Hit Points class and use it within Character.
    Ignore pylint instance attributes warning.

--- dmscreen/models/character.py
+++ dmscreen/models/character.py
@@ -1,7 +1,10 @@
+# pylint: disable=too-many-instance-attributes
+# number of attributes is reasonable in this case
 from enum import Enum
 from dmscreen.models.ability_scores import AbilityScores
 from dmscreen.models.skills import Skills
 from dmscreen.models.classes import Class
+from dmscreen.models.dice import RollType
 
 
 class Alignment(Enum):
@@ -16,6 +19,13 @@ class Alignment(Enum):
     CHAOTIC_EVIL = 8
 
 
+class HitPoints:
+    def __init__(self):
+        self.current = 0
+        self.max = 0
+        self.die = RollType()
+
+
 class Character:
     def __init__(self):
         self._name = ""
@@ -26,8 +36,7 @@ class Character:
         self.speed = 0
         self.ability_score = AbilityScores()
         self.skills = Skills()
-        self.hit_points = 0
-        self.hit_points_max = 0
+        self.hit_points = HitPoints()
         self.proficiency_bonus = 0
         self.passive_wisdom = 0
         self.is_player_character = False
